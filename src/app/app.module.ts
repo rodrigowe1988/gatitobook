5e174da085cdf305195013da8489730dcbd94e97
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-05 19:35:24 -0300

    organizando as rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
