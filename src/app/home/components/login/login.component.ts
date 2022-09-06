ae716a965dfe5deacad487e298c2af4688210701
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-06 10:53:03 -0300

    consumindo servico de autenticacao/ adicionando pasta da API

--- src/app/home/components/login/login.component.ts
+++ src/app/home/components/login/login.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario: string = '';
+  senha: string = '';
 
-  usuario: string = ''
-  senha: string = ''
+  constructor(private authService: AutenticacaoService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-    console.log(this.usuario, this.senha)
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Autenticado com sucesso...');
+      },
+      (error: any) => {
+        alert('usuário não identificado');
+        console.log(error);
+      }
+    );
   }
-
 }
