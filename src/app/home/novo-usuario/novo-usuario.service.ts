d9ad3f6d98a215375f3d9c86bf09d41b7c63d677
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-07 09:40:31 -0300

    validacoes assincronas - verificando se o userName existe no banco de dados

--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NovoUsuario } from './novo-usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NovoUsuarioService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  cadastroNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.http.post('http://localhost:3000/users/signup', novoUsuario)
+  };
+
+  verificarUsuarioExistente(nomeUsuario: string) {
+    return this.http.get(`http://localhost:3000/user/exists/${nomeUsuario}`)
+  }
 }
